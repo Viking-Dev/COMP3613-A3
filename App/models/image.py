88f9ba966ed2a98b6575c93b512cb6ad431e22b9
Jade-Ganga <jonathon.wingson@my.uwi.edu>
2022-11-27 03:07:23 +0000

    Image Model

--- App/models/image.py
+++ App/models/image.py
@@ -3,8 +3,9 @@ from App.database import db
 #Images uploaded by users
 class Image(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    userId =  db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    rankings = db.relationship('Ranking', backref='ranking', lazy=True, cascade="all, delete-orphan")
+    url = db.Column(db.String(120), nullable=False)
+    userId =  db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)                               #owner of the image
+    rankings = db.relationship('Ranking', backref='ranking', lazy=True, cascade="all, delete-orphan")       #other users rank of the image
 
     def __init__(self, userId):
         self.userId = userId
@@ -12,12 +13,8 @@ class Image(db.Model):
     def toJSON(self):
         return{
             'id': self.id,
+            'url': self.url,
             'userId': self.userId,
             'rankings': [ranking.toJSON() for ranking in self.rankings]
         }
 
-#testing hellooooooo
-#testing again-jm
-#testing 2-jg
-#testing to approve trello task -jm
-#demo
